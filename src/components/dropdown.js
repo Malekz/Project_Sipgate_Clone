6ebb7956ba099b567c068cae00a4d158328ee7b3
Malek Zarzour <malek.zarzour@gmail.com>
2020-09-09 16:37:17 +0200

    dropdown with react hoocks

---
+++ src/components/dropdown.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { menuItems } from './menuItems';
+import { Link } from 'react-router-dom';
+
+const DropDown = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+      >
+        {menuItems.map((items, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className={items.cName}
+                to={items.path}
+                onClick={() => setClick(false)}
+              >
+                {items.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default DropDown;
