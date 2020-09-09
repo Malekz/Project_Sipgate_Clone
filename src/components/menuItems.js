82beddb6cd70008b2f0d24fa2cafae39d8154f76
Malek Zarzour <malek.zarzour@gmail.com>
2020-09-09 16:31:39 +0200

    created dummy dropdown menu items

---
+++ src/components/menuItems.js
@@ -0,0 +1,22 @@
+export const menuItems = [
+  {
+    title: 'Marketing',
+    path: 'Marketing',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Consulting',
+    path: 'Consulting',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Design',
+    path: 'Design',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Development',
+    path: 'Development',
+    cName: 'dropdown-link',
+  },
+];
